22fd299e3ec6b003de8bc32d5b8b4d0a05830ab5
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-05 12:54:31 -0500

    restringido accesos

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RequireAuth from './components/Auth/RequireAuth';
 import MainLayout from '@/components/layout/MainLayout';
 import HomePage from '@/pages/HomePage';
 import AddUserPage from '@/pages/AddUserPage';
 import UsersPage from '@/pages/UsersPage';
 import LoginPage from '@/pages/LoginPage';
+import Forbidden from './pages/Forbidden';
 
 function App() {
     return (
@@ -11,8 +13,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<HomePage />} />
+                    <Route path="login" element={<LoginPage />} />
+
                     <Route path="add-user" element={<AddUserPage />} />
-                    <Route path="users" element={<UsersPage />} />
+                    <Route path="users" element={<RequireAuth requiredRole="ADMIN"> <UsersPage /></RequireAuth>} />
                     <Route path="login" element={<LoginPage />} />
                 </Route>
             </Routes>
