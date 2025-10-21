9c15c19c279d0c9c65f9d0ee679fd5a2d9801c28
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-21 14:02:54 -0500

    immportado fichero de config

--- react-app/src/pages/LoginPage.jsx
+++ react-app/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { API_URL } from '../config.jsx';
+import { API_URL } from '../config';
 
 
 const LoginPage = () => {
