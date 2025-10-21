1c235b7deabf82d0acd37197fbd0afb17c4b50a7
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-21 12:44:49 -0500

    agregada configuracion de url global

--- react-app/src/components/Users/UserList.jsx
+++ react-app/src/components/Users/UserList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { API_URL } from './config';
+import { API_URL } from '../../config';
 
 const useUserList = () => {
     const [usuarios, setUsuarios] = useState([]);
